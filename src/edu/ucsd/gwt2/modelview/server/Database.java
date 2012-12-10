8c6b5d65f7f0e22e5aa3d2d8b67e5d1827698c66
coderforlife <jeff@coderforlife.com>
2012-12-09 23:16:27 -0800

    Updating Database class to support external params.

--- src/edu/ucsd/gwt2/modelview/server/Database.java
+++ src/edu/ucsd/gwt2/modelview/server/Database.java
@@ -1,57 +1,20 @@
 package edu.ucsd.gwt2.modelview.server;
 
-//import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 //import java.util.PropertyResourceBundle;
 //import java.util.ResourceBundle;
 
+/**
+ * Database access class. Most of this is deferred to the connection pool.
+ * It requires a class called DBParams with constant strings URL, USERNAME, and PASSWORD.
+ * @author Jeffrey Bush
+ */
 public class Database
 {
-	// setup connection values to the database
-	public static final String DB_DRIVER = "org.postgresql.Driver";
-	public static String URL = "jdbc:postgresql://dev-db.crbs.ucsd.edu:5432/ccdbv2_2";
-	public static String USERNAME = "ccdbd_dev";
-	public static String PASSWORD = "Test753";
-	public static ConnectionPool pool = new ConnectionPool(URL, USERNAME, PASSWORD);
+	// Setup connection values to the database
+	private final static ConnectionPool pool = new ConnectionPool(DBParams.URL, DBParams.USERNAME, DBParams.PASSWORD);
 
-    // Load the driver when this class is first loaded
-    static
-    {
-        try
-        {
-            Class.forName(DB_DRIVER).newInstance();
-        }
-        catch (ClassNotFoundException cnfx) { cnfx.printStackTrace(); }
-        catch (IllegalAccessException iaex) { iaex.printStackTrace(); }
-        catch (InstantiationException iex) { iex.printStackTrace(); }
-    }
-
-//	/**
-//	 * Read the database configuration from a resource file.
-//	 * @param path path to the resource file
-//	 * @throws Exception
-//	 */
-//	public static void readConfig(String path) throws Exception
-//	{
-//  	try
-//		{
-//			//Now, go get the connection info from resource files
-//			//Then get the real connection info from the real resource
-//			ResourceBundle rb = new PropertyResourceBundle(new FileInputStream(path));
-//			USERNAME = rb.getString("postgres_user");
-//			PASSWORD = rb.getString("postgres_password");
-//			URL = rb.getString("postgres_jdbc_url"); //connectString+HOST+":"+PORT+":"+DATABASENAME;
-//			Database.pool.closeConnections();
-//			Database.pool = new ConnectionPool(URL, USERNAME, PASSWORD);
-//		}
-//		catch (Exception e)
-//		{
-//			e.printStackTrace();
-//			throw new Exception("Problem reading database parameters: "+e.getMessage());
-//		}
-//	}
-    
     /**
      * Gets a connection to the database
      * @return a new or reused connection
